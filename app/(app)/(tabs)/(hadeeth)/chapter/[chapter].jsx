ff9d5d5b14f785c9ef657d78e0c73095d742c3ef
mdchad <irsyad.muhd@gmail.com>
2023-03-14 16:08:58 +0800

    merge conflict

--- app/(app)/(tabs)/(hadeeth)/chapter/[chapter].jsx
+++ app/(app)/(tabs)/(hadeeth)/chapter/[chapter].jsx
@@ -1,39 +1,36 @@
 import { Link, useSearchParams } from "expo-router"
-import { FlatList, SectionList, Text, View } from "react-native"
-import data from '@data/hadeeth.json'
-import { useEffect } from "react";
+import { ScrollView, Text, View} from "react-native"
 
 const hadeethContent = () => {
   const { chapter, data, category } = useSearchParams();
   const parsedData = data ? JSON.parse(data) : {}
 
   return (
-    <View className="flex items-center px-6">
-      <Text className="text-3xl font-bold">{category}</Text>
-      <Text className="text-lg font-bold">Chapter: {chapter}</Text>
-      <Text className="text-md mb-2">{parsedData?.title.ar}</Text>
-      <Text className="text-md">{parsedData?.title.ms}</Text>
+    <ScrollView>
+      <View className="flex items-center px-6">
+        <Text className="text-3xl font-bold">{category}</Text>
+        <Text className="text-lg font-bold">Chapter: {chapter}</Text>
+        <Text className="text-md mb-2">{parsedData?.title?.ar}</Text>
+        <Text className="text-md">{parsedData?.title?.ms}</Text>
 
-      {/* create a next and prev chapter */}
-      <View className="my-8">
-        <FlatList
-          data={parsedData?.hadeeth}
-          renderItem={({ item }) => (
-            <>
-              <Text className="mb-4">{item.content.ar}</Text>
-              <Text>{item.content.ms}</Text>
-            </>
-          )}
-          keyExtractor={item => item.id}
-        />
-      </View>
-      <Link href={`Content/Hadeeth/chapter/${parseInt(chapter) + 1}`}>
-        <Text>Next Chapter</Text>
-      </Link>
+        {/* create a next and prev chapter */}
+        <View className="my-8">
+          {parsedData?.hadeeth?.map(hadeeth => {
+            return (
+              <View key={hadeeth.id}>
+                <Text className="mb-4">{hadeeth.content.ar}</Text>
+                <Text className="mb-4">{hadeeth.content.ms}</Text>
+              </View>
+            )
+          })}
+        </View>
+        <Link href={`Content/Hadeeth/chapter/${parseInt(chapter) + 1}`}>
+          <Text>Next Chapter</Text>
+        </Link>
 
-      <Link href={`Content/Hadeeth/chapter/${parseInt(chapter) - 1}`}>
-        <Text>Prev Chapter</Text>
-      </Link>
+        <Link href={`Content/Hadeeth/chapter/${parseInt(chapter) - 1}`}>
+          <Text>Prev Chapter</Text>
+        </Link>
 
       <Link href="../">Back</Link>
     </View>
