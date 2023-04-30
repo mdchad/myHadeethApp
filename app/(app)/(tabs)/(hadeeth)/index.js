dc9d387cde7a97242666acbbf8b39b4c6594cfb6
mdchad <irsyad.muhd@gmail.com>
2023-05-01 03:15:28 +0800

    fix crash issue

--- app/(app)/(tabs)/(hadeeth)/index.js
+++ app/(app)/(tabs)/(hadeeth)/index.js
@@ -1,34 +1,38 @@
 import { View, Text, TouchableWithoutFeedback, Keyboard, Image, ScrollView, useWindowDimensions, StyleSheet, FlatList } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-import { FontAwesome5, AntDesign } from '@expo/vector-icons';
 import books from '@data/books.json'
+import initials from "initialism";
 
 const Hadeeth = () => {
     const { fontScale } = useWindowDimensions();
     const styles = makeStyles(fontScale);
 
     const Item = ({ title, id }) => (
-        <Link href={`(hadeeth)/category/${id}?title=${title}`}>
-            <View className="space-x-3 flex flex-row font-xl p-3 justify-center items-center">
-                <FontAwesome5 name="book" size={16} color="black" />
-                <Text style={styles.title}>{title}</Text>
-            </View>
-        </Link >
+      <View className="bg-white mb-4 rounded-xl py-2">
+          <Link href={`(hadeeth)/category/${id}?title=${title}`}>
+              <View className="space-x-3 flex flex-row font-xl p-3 justify-center items-center">
+                  <View className="bg-[#dad873] rounded-xl w-12 h-12 flex items-center justify-center">
+                      <Text className="text-white">{initials(title, 2)}</Text>
+                  </View>
+                  {/*<FontAwesome5 name="book" size={16} color="black" />*/}
+                  <Text style={styles.title}>{title}</Text>
+              </View>
+          </Link >
+      </View>
     );
 
     return (
-        <View className="flex-1 flex space-y-3 bg-white">
-
-            <View className="flex-1">
-                <FlatList
-                    data={books}
-                    renderItem={({ item }) => <Item title={item.title} id={item.id} />}
-                    keyExtractor={item => item.id}
-                    className="p-3 space-y-10"
-                />
-            </View>
-        </View>
+      <View className="flex-1 bg-gray-100">
+          <View className="">
+              <FlatList
+                data={books}
+                renderItem={({ item }) => <Item title={item.title} id={item.id} />}
+                keyExtractor={item => item.id}
+                className="p-4"
+              />
+          </View>
+      </View>
     );
 };
 
--- app/components/header.js
+++ app/components/header.js
@@ -1,26 +1,50 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-import { FontAwesome5, AntDesign } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { User } from "lucide-react-native";
 
 const header = ({ user }) => {
 
   return (
-    <View className="flex flex-row justify-between items-center px-8 py-6 shadow-lg rounded-b-2xl rounded-bl-2xl bg-[#EDEEC0] overflow-hidden">
-      <View className="">
-        <Text className="mb-1">
-          Assalamualaikum,
-        </Text>
-        <Text className="font-bold uppercase">
-          {user}
-        </Text>
-      </View>
-      <Link href="/modal">
-        <View className="flex flex-row items-center">
-          <AntDesign name="search1" size={16} color="black" />
-          {/* <Image source={require("@assets/search.png")} style={{ width: 16, height: 20 }} /> */}
+    <View className="flex flex-row justify-between items-center py-6 shadow-lg rounded-b-2xl rounded-bl-2xl bg-[#EDEEC0] overflow-hidden">
+      {user ? (
+        <View className="pl-6 flex flex-row">
+          <View className="w-12 h-12 mr-2 bg-white rounded-full flex items-center justify-center">
+            <User color="black"/>
+          </View>
+          <View>
+            <Text className="mb-0.5 text-xl">
+              Assalamualaikum,
+            </Text>
+            <Text className="font-bold">
+              {user}
+            </Text>
+          </View>
         </View>
-      </Link>
+      ) : (
+        <Link href={"SignIn"}>
+          <View className="pl-6 flex flex-row">
+            <View className="w-12 h-12 mr-2 bg-white rounded-full flex items-center justify-center">
+              <User color="black"/>
+            </View>
+            <View>
+              <Text className="mb-0.5 text-xl">
+                Assalamualaikum,
+              </Text>
+              <Text className="font-bold">
+                Guest user
+              </Text>
+            </View>
+          </View>
+        </Link>
+      )}
+      <View className="flex flex-row items-center">
+        <Link href="/modal" className="py-2 px-8">
+            <AntDesign name="search1" size={20} color="black" />
+            {/* <Image source={require("@assets/search.png")} style={{ width: 16, height: 20 }} /> */}
+        </Link>
+      </View>
     </View>
   )
 }
