23a92a7e7428422d1046072013911a193e8b4cf2
Aliy Akhbar <aliyakhbar@gmail.com>
2023-05-07 21:56:35 +0800

    Feature/myh 8 google auth (#33)
    * add clerk provider
    
    * Adding clerk
    
    * added clerk
    
    * remove view
    
    ---------
    
    Co-authored-by: mdchad <irsyad.muhd@gmail.com>

--- app/(app)/(tabs)/(hadeeth)/_layout.jsx
+++ app/(app)/(tabs)/(hadeeth)/_layout.jsx
@@ -1,19 +1,19 @@
 import { Slot, Stack, Tabs } from "expo-router";
 import { Keyboard, Text, TouchableWithoutFeedback, View } from "react-native";
 import Page from '@components/page'
-import { useAuth } from "@context/auth";
+import { useUser } from "@clerk/clerk-expo";
 import Header from '@components/header';
 
 export const unstable_settings = {
-  initialRouteName: "index"
+    initialRouteName: "index"
 }
 
 export default function Layout() {
-    const { user } = useAuth()
+    const { isLoaded, isSignedIn, user } = useUser();
 
     return (
         <Page class="bg-[#EDEEC0]">
-            <Header user={user?.full_name} />
+            <Header user={user} />
             <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen
                     name="index"
