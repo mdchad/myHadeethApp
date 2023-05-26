c9a750a83a703880587b79cbb677591d004d1b6c
mdchad <irsyad.muhd@gmail.com>
2023-05-26 17:17:28 +0800

    add switch for notification

--- app/(app)/settings/index.js
+++ app/(app)/settings/index.js
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react";
 import {
-    StyleSheet,
     View,
-    Alert,
-    Pressable,
     Text,
-    TextInput,
-    SafeAreaView,
-    Image,
     ScrollView,
+    Switch
 } from "react-native";
 
 import Toast from "react-native-root-toast";
 import * as Updates from 'expo-updates';
 import Page from "@components/page";
-import {Info, LogIn, MessageSquare, Smartphone, User} from "lucide-react-native";
+import {BellRing, Hourglass, Info, LogIn, MessageSquare, Smartphone, User} from "lucide-react-native";
 import { Link, useRouter } from "expo-router";
 import { TouchableHighlight } from "react-native-gesture-handler";
 import { ClerkProvider, SignedIn, SignedOut, useAuth } from "@clerk/clerk-expo";
 import * as SecureStore from "expo-secure-store";
+import { View as MotiView, AnimatePresence } from 'moti';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 const tokenCache = {
     getToken(key) {
@@ -39,8 +37,36 @@ const tokenCache = {
 
 export default function Settings() {
     const router = useRouter()
+    const [isEnabled, setIsEnabled] = useState(false);
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('time');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        if (Platform.OS === 'android') {
+            setShow(false);
+            // for iOS, add a button that closes the picker
+        }
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
 
     const { isLoaded, isSignedIn, signOut } = useAuth();
+
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     const triggerUpdate = async () => {
         try {
@@ -63,35 +89,79 @@ export default function Settings() {
         router.push('/(hadeeth)')
     }
 
-    function onPressProfile() {
-        router.push('/profile')
-    }
-
     return (
         <Page class="bg-gray-100">
             <ScrollView className="py-4">
                 <View className="px-4 bg-gray-100 flex sm:mx-auto sm:w-full sm:max-w-md w-full h-full">
                     <View className="bg-white rounded-xl flex">
-                        {/*<SignedIn>*/}
-                        {/*    <TouchableHighlight onPress={onPressProfile} className="bg-white rounded-t-xl" underlayColor="#f9fafb">*/}
-                        {/*        <View className="mx-5 space-x-3 py-5 flex flex-row items-center border-b border-gray-300">*/}
-                        {/*            <User color={'black'} size={20} />*/}
-                        {/*            <Text className="text-[16px]">Profile</Text>*/}
-                        {/*        </View>*/}
-                        {/*    </TouchableHighlight>*/}
-                        {/*</SignedIn>*/}
-                        <View className="mx-5 space-x-3 py-5 flex flex-row items-center border-b border-gray-300">
-                            <MessageSquare size={20} color={'black'} />
-                            <Text className="text-[16px]">Help and Feedback</Text>
+                        <View className="bg-white rounded-xl space-x-3 px-4 py-2 flex flex-row items-center justify-between w-full">
+                            <View className="space-x-3 items-center flex flex-row">
+                                <BellRing color="black" size={20} />
+                                <Text className="text-lg">Notification</Text>
+                            </View>
+                            <Switch
+                              trackColor={{false: '#d1d5db', true: '#5BC236'}}
+                              thumbColor={isEnabled ? '#FFF' : '#f4f3f4'}
+                              ios_backgroundColor="#d1d5db"
+                              onValueChange={toggleSwitch}
+                              value={isEnabled}
+                            />
                         </View>
-                        <View className="mx-5 space-x-3 py-5 flex flex-row items-center border-b border-gray-300">
+                        <AnimatePresence>
+                            {
+                                isEnabled && (
+                                    <MotiView
+                                      from={{
+                                          opacity: 0,
+                                          scale: 0.9,
+                                      }}
+                                      animate={{
+                                          opacity: 1,
+                                          scale: 1,
+                                      }}
+                                      exit={{
+                                          opacity: 0,
+                                          scale: 0.9,
+                                      }}
+                                      exitTransition={{
+                                          type: 'timing',
+                                          duration: 100,
+                                      }}
+                                      className="bg-white rounded-xl space-x-3 px-4 py-2 flex flex-row items-center justify-between w-full">
+                                        <View className="w-full space-x-3 justify-between items-center flex flex-row">
+                                            <View className="space-x-3 items-center flex flex-row">
+                                                <Hourglass color="black" size={20} />
+                                                <Text className="text-lg">Daily hadith time</Text>
+                                            </View>
+                                            <View>
+                                                <DateTimePicker
+                                                  testID="dateTimePicker"
+                                                  value={date}
+                                                  mode={mode}
+                                                  is24Hour={true}
+                                                />
+                                            </View>
+                                        </View>
+                                    </MotiView>
+                              )
+                            }
+                        </AnimatePresence>
+                    </View>
+                    <Text className="ml-5 mb-4 mt-2 text-gray-600">Show hadith in notification daily</Text>
+                    <View className="bg-white rounded-xl flex">
+
+                        <View className="mx-5 space-x-3 py-3 flex flex-row items-center border-b border-gray-300">
+                            <MessageSquare size={20} color={'black'} />
+                            <Text className="text-lg">Help and Feedback</Text>
+                        </View>
+                        <View className="mx-5 space-x-3 py-3 flex flex-row items-center border-b border-gray-300">
                             <Info size={20} color={'black'} />
-                            <Text className="text-[16px]">About</Text>
+                            <Text className="text-lg">About</Text>
                         </View>
                         <TouchableHighlight onPress={triggerUpdate} className="rounded-b-xl bg-white" underlayColor="#f9fafb">
-                            <View className="mx-5 py-5 space-x-3 flex flex-row items-center">
+                            <View className="mx-5 py-3 space-x-3 flex flex-row items-center">
                                 <Smartphone color={'black'} size={20} />
-                                <Text className="text-[16px]">Update App</Text>
+                                <Text className="text-lg">Update App</Text>
                             </View>
                         </TouchableHighlight>
                     </View>
