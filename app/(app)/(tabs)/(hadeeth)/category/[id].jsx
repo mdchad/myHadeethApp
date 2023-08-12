1af1b026f0c9c534ea335ca2d9ccf2fbb6758a6e
Aliy Akhbar <aliyakhbar@gmail.com>
2023-08-12 21:57:50 +0800

    fix new hadeeth

--- app/(app)/(tabs)/(hadeeth)/category/[id].jsx
+++ app/(app)/(tabs)/(hadeeth)/category/[id].jsx
@@ -1,114 +1,45 @@
 import { Link, useRouter, useSearchParams } from "expo-router"
 import { FlatList, ScrollView, Text, View } from "react-native"
 import chapters from '@data/chapters.json'
+import categories from '@data/categories.json'
 import { ChevronRight } from "lucide-react-native";
 import { TouchableHighlight } from "react-native-gesture-handler";
 import Item from "../../../../components/accordion";
 import Accordion from "../../../../components/accordion";
 let he = require('he');
+import { FlashList } from "@shopify/flash-list";
 
 const hadeethCategory = () => {
-    let groupedArray = []
-    chapters.forEach((item) => {
-        let existingCategory = groupedArray.find(
-            (category) => category.category_id === item.category_id
-        );
-
-        if (existingCategory) {
-            existingCategory.chapters.push({
-                id: item.id,
-                name: item.name,
-                title: item.title
-            });
-        } else {
-            groupedArray.push({
-                category_id: item.category_id,
-                category_name: item.category_name,
-                category_title: item.category_title,
-                book_id: item.book_id,
-                book_name: item.book_name,
-                book_title: item.book_title,
-                chapters: [{
-                    id: item.id,
-                    name: item.name,
-                    title: item.title,
-                }]
-            });
-        }
-    })
-
     const { title, id } = useSearchParams();
     const router = useRouter();
 
-    function onPressHadith(chapter) {
+    function onPressHadith(categories) {
         return router.push({
-            pathname: `/(hadeeth)/content/${chapter.id}`,
+            pathname: `/(hadeeth)/content/${categories.id}`,
             params: {
-                chapterId: chapter.id,
+                categoriesId: categories.id,
+                bookId: categories.book_id,
             }
         })
     }
 
     return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View className="flex-1 space-y-6 flex bg-gray-100 px-6 pt-6">
-            {/*<Text className="text-3xl font-bold text-center">{title}</Text>*/}
-            <View className="text-center flex">
-                {groupedArray.map((item, key) => {
-                    if (item.book_id === id) {
-                        return (
-                          <Accordion key={key} itemKey={key} title={item.category_title.ms}>
-                              {item.chapters.map(chapter => {
-                                      return (
-                                        <TouchableHighlight key={chapter.id} onPress={() => onPressHadith(chapter)} underlayColor="#f9fafb" className="rounded-xl w-full">
-                                            <View key={chapter.id} className="px-4 py-4">
-                                                <View className="flex flex-row justify-between items-center w-full">
-                                                    <Text className="w-[90%]">{he.decode(chapter.title.ms)}</Text>
-                                                </View>
-                                            </View>
-                                        </TouchableHighlight>
-                                      )
-                                  })
-                              }
-                          </Accordion>
-                        )
-                    }
-                })}
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <View className="flex-1 space-y-6 flex bg-gray-100 px-6 pt-6">
+                <FlashList
+                    className="space-y-6"
+                    data={categories}
+                    renderItem={({ item }) => <TouchableHighlight key={item.id} onPress={() => onPressHadith(item)} underlayColor="#f9fafb" className="rounded-xl w-full mb-3 overflow-hidden">
+                        <View key={item.id} className="px-4 py-4 bg-white">
+                            <View className="flex flex-row justify-between items-center w-full">
+                                <Text className="capitalize text-md font-semibold">{he.decode(item.title.ms)}</Text>
+                            </View>
+                        </View>
+                    </TouchableHighlight>}
+                    estimatedItemSize={100}
+                />
             </View>
-            {/*    <FlatList*/}
-            {/*        className="h-full"*/}
-            {/*        data={groupedArray.filter(chapter => chapter.book_id === id)}*/}
-            {/*        renderItem={({ item, index }) => {*/}
-            {/*            return (*/}
-            {/*              <Item index={index} title={item.category_title.ms}>*/}
-            {/*                  {*/}
-            {/*                      item.chapters.map(chapter => {*/}
-            {/*                          return (*/}
-            {/*                            <TouchableHighlight key={chapter.id} onPress={() => onPressHadith(chapter)} underlayColor="#f9fafb" className="rounded-xl w-full">*/}
-            {/*                                <View key={chapter.id} className="px-4 py-4 border-t border-t-gray-200">*/}
-            {/*                                    <View className="flex flex-row justify-between items-center w-full">*/}
-            {/*                                        <Text className="w-[90%]">{he.decode(chapter.title.ms)}</Text>*/}
-            {/*                                        <ChevronRight color="black" size={16} />*/}
-            {/*                                    </View>*/}
-            {/*                                </View>*/}
-            {/*                            </TouchableHighlight>*/}
-            {/*                          )*/}
-            {/*                      })*/}
-            {/*                  }*/}
-            {/*              </Item>*/}
-            {/*                // <View className="flex" key={item.category_id}>*/}
-            {/*                //     <View className="">*/}
-            {/*                //         <Text className="my-4 text-gray-800 font-semibold">{item.category_title.ms}</Text>*/}
-            {/*                //     </View>*/}
-
-            {/*                // </View>*/}
-            {/*            )*/}
-            {/*        }}*/}
-            {/*        keyExtractor={item => item.category_id}*/}
-            {/*    />*/}
-            {/*</View>*/}
-        </View>
-      </ScrollView>
+        </ScrollView>
     )
 }
 
--- app/(app)/(tabs)/(hadeeth)/content/[id].jsx
+++ app/(app)/(tabs)/(hadeeth)/content/[id].jsx
@@ -1,87 +1,91 @@
 import { useSearchParams } from "expo-router"
 import { Text, View, ScrollView, Share } from "react-native"
 import hadeeths from '@data/hadeeths.json'
-import {TextInput} from "react-native";
-import React, {useEffect, useState} from "react";
-import {Bookmark, Share as ShareIcon} from "lucide-react-native";
-import {TouchableHighlight} from "react-native";
+import { TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Bookmark, Share as ShareIcon } from "lucide-react-native";
+import { TouchableHighlight } from "react-native";
+
+import hadeethsBukhari from "@data/hadeethsBukhari";
+import hadeethsBukhari2 from "@data/hadeethsBukhari2";
 
 const hadeethContent = () => {
-  const { chapterTitle, chapterId } = useSearchParams();
-  const [hadeeth, setHadeeth] = useState(null)
+    const { chapterTitle, categoriesId } = useSearchParams();
 
-  useEffect(() => {
-    setHadeeth(hadeeths.filter(h => h.chapter_id === chapterId))
-  }, [chapterId])
+    const [hadeeth, setHadeeth] = useState(null)
+    const [book, setBook] = useState(null)
 
-  function onShare(hadeeth) {
-    const message = `\n${hadeeth.content.ar}\n\n ${hadeeth.content.ms}\n\n\n${hadeeth.book_title}\n\n\nwww.myhadeeth.com.my`;
+    useEffect(() => {
+        setHadeeth(hadeethsBukhari.filter(hadeeth => hadeeth.volume_id === categoriesId))
+    }, [categoriesId])
 
-    Share.share({
-      message: message,
-    })
-      .then(result => {
-        if (result.action === Share.sharedAction) {
-          if (result.activityType) {
-            console.log(`Shared with activity type: ${result.activityType}`);
-          } else {
-            console.log('Shared');
-          }
-        } else if (result.action === Share.dismissedAction) {
-          console.log('Dismissed');
-        }
-      })
-      .catch(error => {
-        console.log('Error sharing:', error);
-      });
-  }
+    function onShare(hadeeth) {
+        const message = `\n${hadeeth.content.ar}\n\n ${hadeeth.content.ms}\n\n\n${hadeeth.book_title}\n\n\nwww.myhadeeth.com.my`;
 
-  function onSave() {
-    return null
-  }
+        Share.share({
+            message: message,
+        })
+            .then(result => {
+                if (result.action === Share.sharedAction) {
+                    if (result.activityType) {
+                        console.log(`Shared with activity type: ${result.activityType}`);
+                    } else {
+                        console.log('Shared');
+                    }
+                } else if (result.action === Share.dismissedAction) {
+                    console.log('Dismissed');
+                }
+            })
+            .catch(error => {
+                console.log('Error sharing:', error);
+            });
+    }
 
-  return (
-    <ScrollView>
-      <View className="bg-gray-100 flex items-center px-6 pt-6 space-y-12">
-        {/*<Header user={user.full_name} /> */}
-        <View>
-          <Text className="text-center text-3xl mb-2 font-semibold" style={{ fontFamily: 'Traditional_ArabicBold'}}>{hadeeth ? hadeeth[0]?.title.ar : ''}</Text>
-          <Text className="text-center text-lg font-semibold">{hadeeth ? hadeeth[0]?.title.ms : ''}</Text>
-        </View>
+    function onSave() {
+        return null
+    }
 
-        {/* create a next and prev chapter */}
-        <View className="mt-4 space-y-6">
-          {hadeeth?.map(hadeeth => {
-            return (
-              <View className="px-4 py-6 space-y-4 rounded-xl bg-white" key={hadeeth.id}>
-                {/*<Text className="mb-4 text-right text-2xl" selectable={true} style={{ fontFamily: 'Traditional_ArabicRegular'}}>{hadeeth.content.ar}</Text>*/}
-                <TextInput className="text-gray-800 text-right text-3xl" style={{ fontFamily: 'Traditional_ArabicRegular'}} scrollEnabled={false} readOnly multiline value={hadeeth.content.ar} />
-                <TextInput className="text-gray-800 text-xl overflow-hidden" scrollEnabled={false} readOnly multiline value={hadeeth.content.ms} />
-                <View className="flex flex-row justify-end items-center">
-                  <TouchableHighlight
-                    className="p-2"
-                    underlayColor="#f9fafb"
-                    onPress={() => onSave(hadeeth)}
-
-                  >
-                    <Bookmark color="black" size={24}/>
-                  </TouchableHighlight>
-
-                  <TouchableHighlight
-                    className="p-2"
-                    underlayColor="#f9fafb"
-                    onPress={() => onShare(hadeeth)}
-                  >
-                    <ShareIcon color="black" size={24} />
-                  </TouchableHighlight>
+    return (
+        <ScrollView>
+            <View className="bg-gray-100 flex items-center px-6">
+                {/*<Header user={user.full_name} /> */}
+                <View>
+                    {/* <Text className="text-center text-3xl mb-2 font-semibold" style={{ fontFamily: 'Traditional_ArabicBold' }}>{hadeeth ? hadeeth[0]?.title.ar : ''}</Text> */}
+                    <Text className="text-center text-lg font-semibold py-4">{hadeeth ? hadeeth[0]?.volume_title.ms : ''}</Text>
                 </View>
-              </View>
-            )
-          })}
-        </View>
-      </View>
-    </ScrollView>
-  )
+
+                <View className="space-y-6">
+                    {hadeeth?.map(hadeeth => {
+                        return (
+                            <View className="px-4 py-6 space-y-4 rounded-xl bg-white" key={hadeeth.id}>
+                                {/*<Text className="mb-4 text-right text-2xl" selectable={true} style={{ fontFamily: 'Traditional_ArabicRegular'}}>{hadeeth.content.ar}</Text>*/}
+                                <TextInput className="text-gray-800 text-right text-3xl" style={{ fontFamily: 'Traditional_ArabicRegular' }} scrollEnabled={false} readOnly multiline value={hadeeth.content.ar} />
+                                <TextInput className="text-gray-800 text-xl overflow-hidden leading-loose" scrollEnabled={false} readOnly multiline value={hadeeth.content.ms} />
+                                <View className="flex flex-row justify-end items-center">
+                                    <TouchableHighlight
+                                        className="p-2"
+                                        underlayColor="#f9fafb"
+                                        onPress={() => onSave(hadeeth)}
+
+                                    >
+                                        <Bookmark color="black" size={24} />
+                                    </TouchableHighlight>
+
+                                    <TouchableHighlight
+                                        className="p-2"
+                                        underlayColor="#f9fafb"
+                                        onPress={() => onShare(hadeeth)}
+                                    >
+                                        <ShareIcon color="black" size={24} />
+                                    </TouchableHighlight>
+                                </View>
+                            </View>
+                        )
+                    })}
+                </View>
+            </View>
+        </ScrollView>
+    )
 }
 
 export default hadeethContent
