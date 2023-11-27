b63d2a1eec67226d5a28501ebce70d014dc56d95
mdchad <irsyad.muhd@gmail.com>
2023-11-27 20:47:06 +0800

    add cosmetic

--- app/(app)/(tabs)/(default)/index.js
+++ app/(app)/(tabs)/(default)/index.js
@@ -3,11 +3,12 @@ import {
   Text,
   ScrollView,
   useWindowDimensions,
-  StyleSheet
+  StyleSheet, TouchableHighlight
 } from 'react-native'
 import React from 'react'
 import { FontAwesome5 } from '@expo/vector-icons'
 import Page from '@components/page'
+import {ArrowRight, Bookmark, Heart, Share2} from "lucide-react-native";
 // import Header from '@components/header';
 
 const Home = () => {
@@ -18,67 +19,124 @@ const Home = () => {
     <Page class="bg-white">
       <View className="flex-1 flex space-y-3 bg-white">
         <ScrollView>
-          <View className="p-3 flex items-center space-y-10">
-            <View className="mx-5 space-y-5">
+          <View className="mx-4 p-3 flex space-y-10">
+            <View className="space-y-5">
               <Text
-                className={`text-${
-                  100 / fontScale
-                } font-bold text-center leading-none tracking-tight mx-5`}
-                style={styles.sectionTitle}
+                className={`text-3xl text-royal-blue font-bold leading-none tracking-tight`}
               >
-                Hadeeth of the Day
+                Daily hadith
               </Text>
 
-              <View className="bg-[#DAD8734D] space-y-3 p-5 rounded-md">
-                <Text className="font-bold">Sahih Muslim - Kitab Faraid</Text>
-                <Text>
-                  L(1614) حَدَّثَنَا يَحْيَى بْنُ يَحْيَى، وَأَبُو بَكْرِ بْنُ
-                  أَبِي شَيْبَةَ، وَإِسْحَاقُ بْنُ إِبْرَاهِيمَ، وَاللَّفْظُ
-                  لِيَحْيَى، قَالَ يَحْيَى: أَخْبَرَنَا، وقَالَ الْآخَرَانِ:
-                  حَدَّثَنَا ابْنُ عُيَيْنَةَ، عَنِ الزُّهْرِيِّ، عَنْ عَلِيِّ
-                  بْنِ حُسَيْنٍ، عَنْ عَمْرِو بْنِ عُثْمَانَ، عَنْ أُسَامَةَ
-                  بْنِ زَيْدٍ، أَنَّ النَّبِيَّ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ،
-                  قَالَ: لَا يَرِثُ الْمُسْلِمُ الْكَافِرَ، وَلَا يَرِثُ
-                  الْكَافِرُ
-                </Text>
-              </View>
-
-              <View className="flex flex-row space-x-3 self-end">
-                <Text>Bookmark this hadeeth</Text>
-                <FontAwesome5 name="bookmark" size={16} color="black" />
-              </View>
-            </View>
-
-            <View className="flex items-center justify-center">
-              <Text className="text-2xl text-center">
-                EXPLORE RELATED HADEETH
-              </Text>
-
-              <View className="">
-                <View className="flex flex-row justify-between space-x-6">
-                  <View className="flex items-center">
-                    <View className="mt-4 w-16 h-16 bg-[#D9D9D9] rounded-md"></View>
-                    <Text className="mt-1 uppercase text-xs">Sadqah</Text>
+              <View className="bg-white border border-1 border-royal-blue space-y-3 rounded-md">
+                <View className="p-3">
+                  <Text className="font-bold">Sahih Muslim - Kitab Faraid</Text>
+                  <Text></Text>
+                </View>
+                <View className="flex flex-row justify-between items-center bg-royal-blue">
+                  <View className="flex flex-row items-center">
+                    <TouchableHighlight
+                      className="p-1"
+                      underlayColor="#333"
+                    >
+                      <Share2
+                        color="white"
+                        absoluteStrokeWidth={2}
+                        size={16}
+                      />
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                      className="p-1"
+                      underlayColor="#333"
+                    >
+                      <Heart
+                        color="white"
+                        absoluteStrokeWidth={2}
+                        size={16}
+                      />
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                      className="p-1"
+                      underlayColor="#333"
+                    >
+                      <Bookmark
+                        color="white"
+                        absoluteStrokeWidth={2}
+                        size={16}
+                      />
+                    </TouchableHighlight>
                   </View>
-                  <View className="flex items-center">
-                    <View className="mt-4 w-16 h-16 bg-[#D9D9D9] rounded-md"></View>
-                    <Text className="mt-1 uppercase text-xs">Prayer</Text>
-                  </View>
-                  <View className="flex items-center">
-                    <View className="mt-4 w-16 h-16 bg-[#D9D9D9] rounded-md"></View>
-                    <Text className="mt-1 uppercase text-xs">Family</Text>
-                  </View>
-                  <View className="flex items-center">
-                    <View className="mt-4 w-16 h-16 bg-[#D9D9D9] rounded-md"></View>
-                    <Text className="mt-1 uppercase text-xs">Lifestyle</Text>
+                  <View className="flex flex-row items-center space-x-2">
+                    <Text className="text-white">View More</Text>
+                    <ArrowRight size={18} color={'white'}/>
                   </View>
                 </View>
               </View>
             </View>
 
-            {/* <Link href="Content/Hadeeth/1">Hadeeth 1</Link> */}
-            {/* <Link href="Content/Hadeeth/2">Hadeeth 2</Link> */}
+            <View className="flex flex-row space-x-2">
+              <View className="border border-royal-blue flex items-center rounded-md">
+                <View className="p-2">
+                  <Text className="text-sm text-royal-blue">Forty Hadiths</Text>
+                  <Text className="text-xs text-royal-blue">Forty known famous hadith</Text>
+                </View>
+                <View className="h-[16px] w-full bg-royal-blue">
+                </View>
+              </View>
+              <View className="border border-royal-blue flex flex-wrap items-center rounded-md">
+                <View className="p-2">
+                  <Text className="text-sm text-royal-blue">Kutub Sittah</Text>
+                  <Text className="text-xs text-royal-blue">The six most reliable hadith books</Text>
+                </View>
+                <View className="h-[16px] w-full bg-royal-blue">
+                </View>
+              </View>
+            </View>
+            <View className="bg-white border border-1 border-royal-blue space-y-3 rounded-md">
+              <View className="p-3">
+                <Text className="font-bold">Introduction to MyWay app</Text>
+                <Text></Text>
+              </View>
+              <View className="flex flex-row justify-between items-center bg-royal-blue">
+                <View className="flex flex-row items-center">
+                  <TouchableHighlight
+                    className="p-1"
+                    underlayColor="#333"
+                  >
+                    <Share2
+                      color="white"
+                      absoluteStrokeWidth={2}
+                      size={16}
+                    />
+                  </TouchableHighlight>
+                  <TouchableHighlight
+                    className="p-1"
+                    underlayColor="#333"
+                  >
+                    <Heart
+                      color="white"
+                      absoluteStrokeWidth={2}
+                      size={16}
+                    />
+                  </TouchableHighlight>
+                  <TouchableHighlight
+                    className="p-1"
+                    underlayColor="#333"
+                  >
+                    <Bookmark
+                      color="white"
+                      absoluteStrokeWidth={2}
+                      size={16}
+                    />
+                  </TouchableHighlight>
+                </View>
+                <View className="flex flex-row items-center space-x-2">
+                  <Text className="text-white">View More</Text>
+                  <ArrowRight size={18} color={'white'}/>
+                </View>
+              </View>
+            </View>
           </View>
+
         </ScrollView>
       </View>
     </Page>
@@ -109,10 +167,6 @@ const makeStyles = (fontScale) =>
       backgroundColor: '#D0D0D0',
       width: '100%',
       height: '100%'
-    },
-    sectionTitle: {
-      fontSize: 50 / fontScale, // divide the font size by the font scale
-      fontWeight: '600'
     },
     imageTop: {
       height: 200,
