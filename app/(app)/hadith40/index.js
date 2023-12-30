422c2da10f641e0534a388492753186e74c492c2
mdchad <irsyad.muhd@gmail.com>
2023-12-30 22:04:06 +0800

    add slider

--- app/(app)/hadith40/index.js
+++ app/(app)/hadith40/index.js
@@ -8,6 +8,7 @@ import SHARED_TEXT from '../../i18n'
 import {t} from "i18next";
 import hadiths from '../../../data/hadith40.json'
 import {FlashList} from "@shopify/flash-list";
+import Audio from "../../components/audio";
 
 export default function Hadith40() {
   // const yourPdfURL = 'https://my-way-web.vercel.app/hadis40.pdf'
@@ -15,35 +16,43 @@ export default function Hadith40() {
 
   function Items({ item }) {
     return (
-      <View key={item.number} className="p-4 border border-royal-blue mb-4">
-        <Text className="text-xl">HADIS {item.number}</Text>
-        <Text className="text-xl">{item.hadith_title.ms}</Text>
+      <View key={item.number} className="flex space-y-2 break-words p-4 border border-royal-blue mb-4">
+        <View className="flex flex-row gap-2 items-center mb-6">
+          <View className="flex items-center bg-royal-blue rounded-lg p-2 text-center">
+            <Text className="text-white text-xl">Hadis</Text>
+            <Text className="text-white text-xl">{item.number}</Text>
+          </View>
+          <View className="flex-shrink">
+            <Text className="text-2xl">{item.hadith_title.ms}</Text>
+          </View>
+        </View>
         {
           item.content.map((cnt, index) => {
             // console.log(index)
             // console.log(item.narrators[index])
             return (
-              <View key={index}>
-                <Text>{item.narrators[index].ms}</Text>
-                <View>
-                  <Text className="font-bold text-2xl text-right" style={{ fontFamily: 'Traditional_ArabicRegular' }}>{cnt.ar}</Text>
+              <View key={index} className="mb-4">
+                <Text className="mb-4">{item.narrators[index].ms}</Text>
+                <View className="bg-gray-200 rounded-md px-2 py-4">
+                  <Text className="font-bold text-3xl text-right" style={{ fontFamily: 'Traditional_ArabicRegular' }}>{cnt.ar}</Text>
                   <Text className="font-bold text-lg" style={{ fontFamily: 'Traditional_ArabicRegular' }}>{item.narratedBy[index].ar}</Text>
                 </View>
-                <View>
-                  <Text className="font-semibold">{cnt.ms}</Text>
-                  <Text className="text-xs">{item.narratedBy[index].ms}</Text>
+                <View className="mt-4">
+                  <Text className="text-md mb-2">{cnt.ms}</Text>
+                  <Text className="text-xs text-right">{item.narratedBy[index].ms}</Text>
+                  <Audio />
                 </View>
               </View>
             )
           })
         }
-        <View>
-          <Text className="text-xl">Pengajaran</Text>
+        <View className="bg-royal-blue p-4">
+          <Text className="text-white text-xl mb-2 font-semibold">Pengajaran</Text>
           {
             item.lesson.map((l, i) => {
               return (
                 <View key={i}>
-                  <Text>{`\u2022 ${l.ms}`}</Text>
+                  <Text className="text-white text-lg">{`\u2022 ${l.ms}`}</Text>
                 </View>
               )
             })
--- app/components/audio.js
+++ app/components/audio.js
@@ -1,54 +1,90 @@
-import React, { useState } from 'react'
-import { Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Button, View, Text, StyleSheet } from 'react-native'
 import { Audio } from 'expo-av'
+import { Slider } from '@react-native-assets/slider'
 
 const SoundPlayer = () => {
   const [sound, setSound] = useState()
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playbackStatus, setPlaybackStatus] = useState({});
+  const [position, setPosition] = useState(0);
 
-  const playSound = async () => {
-    try {
-      const { sound } = await Audio.Sound.createAsync(
-        require('./../../assets/Hello.mp3'),
-        // { uri: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3' },
-        {
-          shouldPlay: true
-        }
-      )
-      setSound(sound)
-      await sound.playAsync()
-    } catch (error) {
-      console.log('Error playing sound: ', error)
+  useEffect(() => {
+    loadAudio()
+    return sound
+      ? () => {
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, []);
+
+  const loadAudio = async () => {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+      require('./../../assets/audio/hadis-2-malay.mp3'),
+      {
+        shouldPlay: isPlaying,
+      },
+      onPlaybackStatusUpdate
+    );
+    setSound(sound);
+  };
+
+  const onPlaybackStatusUpdate = status => {
+    setPlaybackStatus(status);
+    if (status.isLoaded) {
+      setPosition(status.positionMillis);
     }
-  }
+  };
 
-  const stopSound = async () => {
-    try {
-      await sound.stopAsync()
-      setSound(null)
-    } catch (error) {
-      console.log('Error stopping sound: ', error)
-    }
-  }
-
-  const handleButtonPress = () => {
-    if (sound) {
-      stopSound()
+  const handlePlayPause = async () => {
+    if (!sound) {
+      await loadAudio();
     } else {
-      playSound()
+      if (playbackStatus.isPlaying) {
+        await sound.pauseAsync();
+      } else {
+        await sound.playAsync();
+      }
     }
-  }
+    setIsPlaying(!playbackStatus.isPlaying);
+  };
 
-  Audio.setAudioModeAsync({
-    playsInSilentModeIOS: true,
-    staysActiveInBackground: true
-  })
+  const handleSeek = async value => {
+    if (sound) {
+      await sound.setPositionAsync(value);
+    }
+  };
 
   return (
-    <Button
-      title={sound ? 'Stop Sound' : 'Play Sound'}
-      onPress={handleButtonPress}
-    />
+    <View>
+      <Button
+        title={isPlaying ? 'Stop Sound' : 'Play Sound'}
+        onPress={handlePlayPause}
+      />
+      <Slider
+        value={position}
+        onValueChange={handleSeek}
+        maximumValue={playbackStatus.durationMillis || 0}
+        minimumValue={0}
+        disable={!playbackStatus.isLoaded}
+      />
+      <Text>{`Position: ${position}ms`}</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  slider: {
+    width: 200,
+    height: 40,
+  },
+});
 
 export default SoundPlayer
