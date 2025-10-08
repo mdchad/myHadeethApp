a3aa113d5f140c8be8eacfb11effef454c28b5d2
mdchad <irsyad.muhd@gmail.com>
2025-10-08 17:03:09 +0800

    add more logging

--- app/components/audio.js
+++ app/components/audio.js
@@ -1,86 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import {View, TouchableOpacity, ActivityIndicator} from 'react-native'
-import { Audio } from 'expo-av'
+import { useAudioPlayer, useAudioPlayerStatus } from 'expo-audio'
 import { Slider } from '@react-native-assets/slider'
 import {Loader, PauseIcon, PlayIcon, StopCircle} from "lucide-react-native";
 
 const SoundPlayer = ({url}) => {
-  const [sound, setSound] = useState()
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [playbackStatus, setPlaybackStatus] = useState({});
-  const [position, setPosition] = useState(0);
+  const audioUri = `https://my-way-web.vercel.app/audio/${url}.mp3`
+  const player = useAudioPlayer(audioUri)
+  const status = useAudioPlayerStatus(player)
 
-  useEffect(() => {
-    // loadAudio().then(() => {
-    // }).catch(e => {
-    //   console.log('eerrrr', e)
-    // })
-    return sound
-      ? () => {
-        console.log('Unloading Sound');
-        sound.unloadAsync();
-      }
-      : undefined;
-  }, [sound]);
-
-  const loadAudio = async () => {
-    const uri = `https://my-way-web.vercel.app/audio/${url}.mp3`
-    try {
-      const { sound, status } = await Audio.Sound.createAsync(
-        // require('../../assets/audio/hadis-2-malay.mp3'),
-        { uri },
-        null,
-        onPlaybackStatusUpdate
-      );
-      setSound(sound);
-      await sound.playAsync()
-    } catch (e) {
-      console.log('errr', e)
-    }
-
-  };
-
-  const onPlaybackStatusUpdate = status => {
-    console.log(status)
-    setPlaybackStatus(status);
-    if (status.isLoaded) {
-      setPosition(status.positionMillis);
-      setIsPlaying(status.isPlaying)
-    }
-  };
-
-  const handlePlayPause = async () => {
-    if (!sound) {
-      await loadAudio();
+  const handlePlayPause = () => {
+    if (status.playing) {
+      player.pause()
     } else {
-      if (playbackStatus.isPlaying) {
-        await sound.pauseAsync();
-      } else {
-        await sound.replayAsync();
+      // If playback ended, seek to beginning before playing
+      if (status.currentTime >= status.duration) {
+        player.seekTo(0)
       }
+      player.play()
     }
-    setIsPlaying(!playbackStatus.isPlaying);
-  };
+  }
 
-  const handleSeek = async value => {
-    if (sound) {
-      await sound.setPositionAsync(value);
-    }
-  };
-
-  // if (!sound) {
-  //   return (
-  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  //       <ActivityIndicator size="small" color="#0000ff" />
-  //     </View>
-  //   )
-  // }
+  const handleSeek = (value) => {
+    // Convert milliseconds to seconds for expo-audio
+    player.seekTo(value / 1000)
+  }
 
   return (
     <View className="rounded-md mt-4 flex flex-row bg-gray-800/90 w-full flex-shrink space-x-4 items-center py-1 px-2">
       <View>
         {
-          isPlaying ? (
+          status.playing ? (
             <TouchableOpacity onPress={handlePlayPause}>
               <PauseIcon size={24} color="white" fill="#fff"/>
             </TouchableOpacity>
@@ -93,7 +43,7 @@ const SoundPlayer = ({url}) => {
       </View>
       <View className="w-full">
         <Slider
-          value={position}
+          value={status.currentTime * 1000}
           style={{
             width: 240
           }}
@@ -102,10 +52,10 @@ const SoundPlayer = ({url}) => {
             backgroundColor: 'rgb(156, 163, 175)'
           }}
           onValueChange={handleSeek}
-          maximumValue={playbackStatus.durationMillis || 0}
+          maximumValue={status.duration * 1000 || 0}
           minimumValue={0}
           thumbSize={14}
-          disable={!playbackStatus.isLoaded}
+          disable={!status.duration}
           thumbTintColor="white"
           trackHeight={3}
         />
