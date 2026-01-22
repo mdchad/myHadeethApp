39f47da09bbdc1eb01524d172e4a1dccb9dde7c3
mdchad <irsyad.muhd@gmail.com>
2026-01-22 12:52:50 +0800

    remove dfault

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -14,7 +14,7 @@ const hadithLogo = require('@/assets/hadith.png')
 const _layout = () => {
   return (
     <Tabs
-      initialRouteName="(default)"
+      initialRouteName="index"
       screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: 'white',
@@ -35,7 +35,7 @@ const _layout = () => {
       }}
     >
         <Tabs.Screen
-          name="(default)"
+          name="index"
           options={{
             tabBarLabel: t(SHARED_TEXT.TAB_HOME_LABEL),
             tabBarIcon: ({ focused }  ) => {
